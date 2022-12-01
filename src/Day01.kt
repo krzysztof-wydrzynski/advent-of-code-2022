f78f576d6334026b66a1e4040149928f7878b5f8
krzysztof.wydrzynski <krzysztof.wydrzynski@zendesk.com>
2022-12-01 10:42:23 +0100

    Day 1 - working solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun sumValuesSeparatedByEnter(input: List<String>): List<Int> =
+        input.fold(mutableListOf(0)) { resultList, next ->
+            if (next == "\n" || next.isBlank()) {
+                resultList.add(0)
+                resultList
+            } else {
+                val last: Int = resultList.removeLast()
+                resultList.add(last + Integer.valueOf(next))
+                resultList
+            }
+        }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    fun part1(input: List<String>) = sumValuesSeparatedByEnter(input).max()
+
+    fun part2(input: List<String>) = sumValuesSeparatedByEnter(input).sortedDescending().subList(0, 3).sum()
+
+    val testInput = readInput("day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
+    val input = readInput("day01_input")
     println(part1(input))
     println(part2(input))
 }
